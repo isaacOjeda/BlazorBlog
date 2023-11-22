972cca76287602a2e14a2173eaf846f4bc23c871
Isaac Ojeda <isaac.ojeda@intelectix.com>
2023-11-22 17:58:22 -0600

    Mejoras del listado de posts

--- src/BlazorBlog/Program.cs
+++ src/BlazorBlog/Program.cs
@@ -108,8 +108,20 @@ async Task SeedData()
             Content = "<p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>\r\n                        <p>Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>\r\n                        <p>What was most significant about the lunar voyage was not that man set foot on the Moon but that they set eye on the earth.</p>\r\n                        <p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>\r\n                        <p>For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us.</p>",
             AuthorId = admin!.Id,
             Created = DateTime.UtcNow,
-            Subtitle = "Problems look mighty small from 150 miles up"
+            Subtitle = "Problems look mighty small from 150 miles up",
+            Slug = "my-first-post"
         });
+
+        context.Posts.Add(new Post
+        {
+            Title = "Science has not yet mastered prophecy",
+            Content = "<p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>\r\n                        <p>Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>\r\n                        <p>What was most significant about the lunar voyage was not that man set foot on the Moon but that they set eye on the earth.</p>\r\n                        <p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>\r\n                        <p>For those who have seen the Earth from space, and for the hundreds and perhaps thousands more who will, the experience most certainly changes your perspective. The things that we share in our world are far more valuable than those which divide us.</p>",
+            AuthorId = admin!.Id,
+            Created = DateTime.UtcNow,
+            Subtitle = "We predict too much for the next year and yet far too little for the next ten",
+            Slug = "my-second-post"
+        });
+
 
         await context.SaveChangesAsync();
     }
